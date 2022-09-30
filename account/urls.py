5e81e0955379f8b0e12d669e5bb8353739bdf739
AnishBade <badeanish6045@gmail.com>
2022-09-30 13:13:06 +0545

    User registration added

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path 
+from django.urls import path,include
 from . import views
 from django.contrib.auth import views as auth_views
-urlpatterns = [ # post views
+urlpatterns = [ 
+    path('', include('django.contrib.auth.urls')),
+    # post views
     # path('login/', views.user_login, name='log  
     path('login/', auth_views.LoginView.as_view(), name='login'), 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
@@ -14,6 +16,6 @@ urlpatterns = [ # post views
     path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'), 
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'), 
     path('reset/done/',auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
+    path('register/', views.register, name='register'),
 ]
 
