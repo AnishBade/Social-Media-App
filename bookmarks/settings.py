616af3d74fb9955ae60ba5d387e14eb1ce0ac383
AnishBade <badeanish6045@gmail.com>
2022-09-30 21:58:45 +0545

    .

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -128,8 +128,8 @@ STATIC_URL = '/static/'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'badeanish6045@gmail.com' 
-EMAIL_HOST_PASSWORD = 'hfskngikroinvurw'
-EMAIL_PORT = 587 
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'badeanish6045@gmail.com' 
+# EMAIL_HOST_PASSWORD = 'hfskngikroinvurw'
+# EMAIL_PORT = 587 
+# EMAIL_USE_TLS = True
